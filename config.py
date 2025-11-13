5be8bd331908a7351cb9d20c2bb469c72e8e6744
music-miko <voilet856@gmail.com>
2025-11-13 14:37:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config(object):
     OWNER_ID = getenv("OWNER_ID", "6848223695")                  # enter your id here
     API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
     VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-    API_KEY = getenv("API_KEY", "30DxNexGenBotse698a9") # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+    API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
     
     # optional config variables
